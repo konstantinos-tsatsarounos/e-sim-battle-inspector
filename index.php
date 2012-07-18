fdc4329320e5752991387c7bbb5705bc26243f06
Konstantinos Tsatsarounos <Konstantinos.Tsatsarounos@gmail.com>
2012-07-18 20:51:43 +0300

    Update master

--- index.php
+++ index.php
@@ -13,16 +13,65 @@
 		html { width:100%;}
 		body { width:95%;}
 		.tbl-centered td, .tbl-centered th { text-align: center;}
-		.form { width: 80%; }
+		.form { width: 100%; }
 		input, label { display: inline-block; padding: 5px; }
+		#ModalWindow {
+			background-color: #fff;
+			width: 500px;
+			height: 200px;
+			border: 1px solid #999;
+			border-radius: 10px;
+			position: absolute;
+			top: 30%;
+			left: 30%;
+			color: #444;
+			font-size: 40px;
+			text-align: center;
+			padding: 10px;
+			display: none;
 
+		}
+		#ModalWindow div { margin: 0 auto; padding: 50px; width: 80%; height: 80%; line-height: 45px;}
 
 	</style>
 	<body>
 		
-		<div class="form">
-			<label>BattleId <input id="BattleId" type="number" value="0" placeholder="Κωδικός μάχης" /></label>
-			<label>RoundId <input id="RoundId" type="number" value="0" placeholder="Αριθμός γύρου" /></label>
+		<div id="ModalWindow">
+			<div></div>
+		</div>
+
+
+		<div class="form" >
+			<label>Εισάγετε τον αριθμό της μάχης<input id="BattleId" type="number"  placeholder="Κωδικός μάχης" /></label>
+			<label>Εισάγετε τον αριθμό του γύρου<input id="RoundId" type="number"  placeholder="Αριθμός γύρου" /></label>
+			<select id="MU" name="MU">
+								<option value="">Επιλέξτε MU
+								<option value="">----------------------------------------
+								<option value="43">Mugiwara Pirates
+								<option value="311">Mugiwara Pirates Academy
+								<option value="36">National Guard
+								<option value="93">Omerta
+								<option value="247">Omerta 2
+								<option value="392">Omerta Academy
+								<option value="63">Blues Brothers
+								<option value="133">Death Squad
+								<option value="335">Jedi Elite
+								<option value="372">Greek Presidential Guard
+								<option value="405">The Expendables
+								<option value="482">The Expendables II
+								<option value="541">Phalanx
+								<option value="565">Titanes
+								<option value="154">The SMURFS
+								<option value="13">The baby SMURFS
+								<option value="194">Libertad
+								<option value="269">Armed Troops
+								<option value="176">Greek Army
+								<option value="253">redbull team
+								<option value="424">AKRITES
+								<option value="642">MARVEL
+							</select>
+			<input type="number" id="SumDamage" value="0" readonly="readonly" placeholder="Συνολο ζημιάς μονάδας" />
+			<input type="button" value="Clear Damage" id="ClearDamage" />
 			<input type="button" id="trigg" value="Display" />
 		</div>
 
@@ -202,6 +251,48 @@
    				 }
 				});
 			}
+
+			$("<span>x</span>", { class: "close"})
+										.css({
+										position: "absolute",
+										top: 10,
+										right:10,
+										fontWeight: "bold",
+										fontSize: 25,
+										"z-index" : 10000,
+										cursor: "pointer"
+
+									})
+									.appendTo("#ModalWindow")
+									.on("click", function(){ 
+										$(this).parent("div").fadeOut(300);
+
+
+									});
+					$("#MU").live("change", function () {    
+    								var MU = this.options[this.selectedIndex].value,
+    								totalDamage=0;
+
+    								for(var i=0; i<fileT.length; i++)
+    								{
+    									if(fileT[i].militaryUnit==MU)
+    									{
+    										totalDamage += fileT[i].damage;
+    									}
+    								}
+
+    								$("#ModalWindow")
+    										.fadeIn(200)
+    										.children("div")
+    										.html("Συνολική ζημιά: "+totalDamage);
+    								var val = document.getElementById("SumDamage").value;
+    								val = parseInt(val);
+
+    								document.getElementById("SumDamage").value = val + parseInt(totalDamage);
+
+   								 });
+					$("#ClearDamage").on("click", function(){ document.getElementById("SumDamage").value = 0 ;})
+
 		</script>
 	</body>
 </html>
